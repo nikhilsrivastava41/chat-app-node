1f392169e4b34fdc0b3bcfbf9bb24813f40b8c2b
Nikhil Srivastava <nikhilsrivastava41@gmail.com>
2018-12-20 08:52:12 +0530

    changes

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 socket.on('connect',function(){
   console.log('connected to server');
 
-  socket.emit('createMessage',{
-    to: "lola@hmail.com",
-    text: 'hey there!'
-  });
 });
 socket.on('disconnect',function(){
   console.log('disconnected from server');
