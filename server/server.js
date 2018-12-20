f0ad6f3ff0b81705ba6589ae5036136b005c77d1
Nikhil Srivastava <nikhilsrivastava41@gmail.com>
2018-12-20 15:29:04 +0530

    geolocation

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message.js');
+const {generateMessage,generateLocationMessage} = require('./utils/message.js');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -21,11 +21,9 @@ io.on('connection', (socket) => {
     io.emit('newMessage',generateMessage(message.from , message.text));
 
     callback('this is form server');
-    // socket.broadcast.emit('newMessage',{
-    //   from: message.from,
-    //   text: message.text,
-    //   createAt: new Date().getTime()
-    // });
+    socket.on('createLocationMessage',(coords)=>{
+      io.emit('newLocationMessage',generateLocationMessage('admin',coords.latitude, coords.longitude));
+    });
   });
   socket.on('disconnect', () => {
     console.log('User was disconnected');
--- server/utils/message.js
+++ server/utils/message.js
@@ -6,7 +6,15 @@ var generateMessage = (from , text)=>{
   };
 
 };
+var generateLocationMessage = (from , latitude , longitude)=>{
+  return {
+    from ,
+    url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+    createAt: new Date().getTime()
+  };
 
+};
 module.exports = {
-  generateMessage
+  generateMessage,
+  generateLocationMessage
 };
